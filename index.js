340c0d2c4060eb04cd5cde75c3e18e15cfc97917
uvrjep8888 <uverojeffreydev@gmail.com>
2026-01-08 09:54:32 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("This is the upside down .... \n");
+});
+
+server.listen(port, () => {
+  // This console.log runs once when the server starts
+  console.log(`Server running at http://localhost:${port}/`);
+});
